3d3a043e1005ab268ef7ef144cb312e60e555579
lakshmiakls <84975931+lakshmiakls@users.noreply.github.com>
2021-05-31 20:19:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+console.log("Hello World!");
+
+// node script.js
+
+// node
+
+// let - block scope, variable
+
+// const - block scope, constant
+
+// var - function scope, variable
+
+var x;
+let y;
+
+x = 5;
+y = 10;
+
+let z = 8;
+
+let i,j,k;
+
+let a=2, b=3, c=4;
+
+console.log(x);
+console.log(i);
+// console.log(h);
+
+x = 9;
+console.log(x);
+
+// Naming variables
+
+let q = 5;
+let _q = 6;
+let $q = 7;
+let q1 = 8;
+
+// let 1q = 3;
+
+console.log(q,_q,$q,q1);
+// console.log(1q);
+
+console.log("a" == "a");
+console.log("a" === "a");
+console.log("a" == "A");
+
+console.log(1 == 1);
+console.log(1 === 1);
+console.log(1 == "1");
+console.log(1 === "1");
+console.log("1" === "1");
+
+console.log(1+1);
+console.log("1"+"1");
+console.log(1-1);
+console.log("1"-"1");
+console.log(1+"1")
+console.log(1-"1");
+
+// let new = 5;
+
+const r = 5;
+console.log(r);
+// r = 8;
+// console.log(r);
+// const s;
+
+const firstName = "Jayesh";
+const lastName = "Sugumaran";
+
+console.log(firstName + lastName);
+
+console.log(firstName + ' ' + lastName);
+
+console.log(`${firstName} ${lastName}`)
